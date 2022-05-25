7cc98a87e340dc8ccf6f17967d28fce1c6196c61
Nathan Black <nathanblack14686@gmail.com>
2022-05-25 12:49:29 -0500

    CMC backend graphs. Formatting needs to be added.

--- cmc_conductivity.py
+++ cmc_conductivity.py
@@ -22,22 +22,23 @@ def determine_cmc(concentrations, conductivities):
     higher_rsqaured_optimized = 0
     higher_linearregion_index_inclusive = lower_linearregion_index_inclusive + 1
     for i in range(3, len(concentrations)):
-        higher_rsquared = statistics.correlation(concentrations[len(concentrations)-1:len(concentrations)-(i+1)])
+        higher_rsquared = statistics.correlation(concentrations[len(concentrations)-(i+1):len(concentrations)-1], 
+        conductivities[len(conductivities)-(i+1):len(conductivities)-1])
         if higher_rsquared >=  higher_rsqaured_optimized:
             higher_rsqaured_optimized = higher_rsquared
             higher_linearregion_index_inclusive = len(concentrations) - i
     lower_slope, lower_intercept = statistics.linear_regression(concentrations[0:lower_linearregion_index_inclusive+1], \
          conductivities[0:lower_linearregion_index_inclusive+1])
-    higher_slope, higher_intercept = statistics.linear_regression(concentrations[len(concentrations)-1:higher_linearregion_index_inclusive-1], \
-         conductivities[len(concentrations)-1:higher_linearregion_index_inclusive-1])
+    higher_slope, higher_intercept = statistics.linear_regression(concentrations[higher_linearregion_index_inclusive:len(concentrations)], \
+         conductivities[higher_linearregion_index_inclusive:len(conductivities)])
     cmc = (higher_intercept - lower_intercept) / (lower_slope - higher_slope)
     return cmc, lower_slope, lower_intercept, lower_rsqaured_optimized, higher_slope, higher_intercept, higher_rsqaured_optimized, lower_linearregion_index_inclusive, higher_linearregion_index_inclusive
 
 def plot_cmc(concentrations, conductivities, cmc, lower_slope, lower_intercept, lower_rsqaured_optimized, higher_slope, \
      higher_intercept, higher_rsqaured_optimized, lower_linearregion_index_inclusive, higher_linearregion_index_inclusive):
      plt.scatter(concentrations,conductivities,color="k")
-     plt.scatter(concentrations[0:lower_linearregion_index_inclusive+1], conductivities[0:lower_linearregion_index_inclusive+1], "r")
-     plt.scatter(concentrations[higher_linearregion_index_inclusive:len(concentrations)], conductivities[higher_linearregion_index_inclusive:len(concentrations)], "g")
+     plt.scatter(concentrations[0:lower_linearregion_index_inclusive+1], conductivities[0:lower_linearregion_index_inclusive+1], color="r")
+     plt.scatter(concentrations[higher_linearregion_index_inclusive:len(concentrations)], conductivities[higher_linearregion_index_inclusive:len(concentrations)], color="g")
      concentration_steprange = concentrations[len(concentrations)-1] - concentrations[0]
      concentration_stepsize = concentration_steprange / 99
      concentration_line_xaxis = []
@@ -48,13 +49,14 @@ def plot_cmc(concentrations, conductivities, cmc, lower_slope, lower_intercept, 
          concentration_line_xaxis.append(current_conc)
          concentration_lowerline_yaxis.append(lower_slope * current_conc + lower_intercept)
          concentration_higherline_yaxis.append(higher_slope * current_conc + higher_intercept)
-     plt.scatter(concentration_line_xaxis, concentration_lowerline_yaxis, "r--")
-     plt.scatter(concentration_line_xaxis, concentration_higherline_yaxis, "g--")
+         current_conc += concentration_stepsize
+     plt.plot(concentration_line_xaxis, concentration_lowerline_yaxis, color="r", linestyle="dashed")
+     plt.plot(concentration_line_xaxis, concentration_higherline_yaxis, color="g", linestyle="dashed")
      cmc_x = []
      cmc_y = []
      cmc_x.append(cmc)
      cmc_y.append(cmc*lower_slope + lower_intercept)
-     plt.scatter(cmc_x, cmc_y, "b")
+     plt.scatter(cmc_x, cmc_y, color="b")
      plt.show()
 
 
